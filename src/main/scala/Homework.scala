f6ead1cefc1f3b10a455b92f78e13f3e1c3e3865
Philip Wills <philip.wills@guardian.co.uk>
2012-08-06 19:18:16 +0100

    Solution for homework, plus a bit more
    I intend to beef up the tests

--- src/main/scala/Homework.scala
+++ src/main/scala/Homework.scala
@@ -4,5 +4,9 @@ object Homework {
   ////////////////////////////////////
 
   val naughtyWords = List("Celery", "Megatron")
-  def isNaughty(text: String) = false
+  val caseInsensitiveWordPattern = """(?i)(^|\s)%s(\s|$)"""
+  val naughtyPatterns = naughtyWords.map(caseInsensitiveWordPattern.format(_).r)
+	
+  // Note that use of isDefined is generally bad-style, but I can't see a better option here
+  def isNaughty(text: String) = naughtyPatterns.exists(_.findFirstIn(text).isDefined)
 }
