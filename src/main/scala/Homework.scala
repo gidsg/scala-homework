dc978c87ec3ddcdc92e0f319455aa204c0fe7810
Philip Wills <philip.wills@guardian.co.uk>
2012-08-07 09:10:06 +0100

    Clarification of comment

--- src/main/scala/Homework.scala
+++ src/main/scala/Homework.scala
@@ -7,6 +7,6 @@ object Homework {
   val wordBoundaryRegexPattern = """(?i)(^|\s)%s(\s|$)""" // The %s is for use with the printf style format method
   val naughtyPatterns = naughtyWords.map(wordBoundaryRegexPattern.format(_).r)
 
-  // Note that use of isDefined is generally bad-style, but I can't see a better option here
+  // Note that use of isDefined is generally not idiomatic (see the scaladoc for Option), but I can't see a better option here
   def isNaughty(text: String) = naughtyPatterns.exists(_.findFirstIn(text).isDefined)
 }
