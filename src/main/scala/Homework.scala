7c2be54dc5b392c6ec7bdf169e201981fd0fcc8d
Philip Wills <philip.wills@guardian.co.uk>
2012-08-08 10:08:20 +0100

    Adding a small second exercise

--- src/main/scala/Homework.scala
+++ src/main/scala/Homework.scala
@@ -7,4 +7,6 @@ object Homework {
   val wordBoundaryRegexPattern = """(?i)(^|\s)%s(\s|$)""" // The %s is for use with the printf style format method
 
   def isNaughty(text: String) = false
+
+  def modulo42(ints: Int*) = Seq()
 }
---
+++ src/test/scala/Homework2.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers._
+
+class Homework2 extends FunSuite {
+
+  test("can use varargs") {
+    Homework.modulo42(43, 63, 97) should be (Seq(1,21,13))
+  }
+
+  test("can use varargs with a collection") {
+    Homework.modulo42(Seq(43, 63)) should be (Seq(1,21))  // Making the test compile is the object of the exercise
+  }
+
+}
