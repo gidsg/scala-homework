0351c7ef1d565bd371007938599db3cb7f8618dc
Philip Wills <philip.wills@guardian.co.uk>
2012-08-15 09:06:38 +0100

    Adding baisc test for defining classes

---
+++ src/test/scala/Homework3.scala
@@ -0,0 +1,26 @@
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers._
+
+class Homework3 extends FunSuite {
+
+  /*
+  test("can create a class") {
+    val coolCat = new CatPlayingPiano("bob")
+    coolCat.name should be ("bob")
+    coolCat.isHowlingAtTheMoon should be (true)
+  }
+
+  test("no two cats are the same") {
+    val myVic = new CatPlayingPiano("vic")
+    val yourVic = new CatPlayingPiano("vic")
+    myVic should not be === (yourVic)
+  }
+
+  test("a number is a number even if it's complex") {
+    val pureImagination = Complex(0, 1)
+    val i = Complex(0, 1)
+
+    pureImagination should be === i
+  }
+  */
+}
